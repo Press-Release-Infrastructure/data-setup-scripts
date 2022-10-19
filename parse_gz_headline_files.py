27783d8ea19e9a9b9fb58279e181221d1415bd9e
Lily Bhattacharjee <lbhattacharjee@berkeley.edu>
2022-10-19 12:05:59 -0700

    Add parsing for indexing terms, publication date, news wire, language, etc.

--- parse_gz_headline_files.py
+++ parse_gz_headline_files.py
@@ -10,6 +10,10 @@ article_id = []
 source_filename = []
 pub_date = []
 news_wire = []
+language = []
+indexing_terms = []
+company_terms = []
+ticker_terms = []
 
 filename = os.path.basename(file_path)
 with gzip.open(file_path, 'rt') as headline_dump:
@@ -40,6 +44,29 @@ with gzip.open(file_path, 'rt') as headline_dump:
             nw = re.search('<publicationName>(.*)</publicationName>', chunk).group(1) 
         except:
             nw = ''
+
+        try:
+            lang = re.search('<classification classificationScheme="language">[\s|\S]*?<className>([a-zA-Z]*)<\/className>[\s|\S]*?<\/classification>', chunk).group(1) 
+        except:
+            lang = ''
+
+        try:
+            indexing_chunk = re.search('<classificationGroup classificationScheme="indexing-terms">([\s|\S]*?)<\/classificationGroup>', chunk).group(1)
+            curr_indexing_terms = re.findall('<classificationItem score="([0-9]*)">[\s|\S]*?<className>(.*?)<\/className>[\s|\S]*?<\/classificationItem>', indexing_chunk)
+        except:
+            curr_indexing_terms = []
+
+        try:
+            company_chunk = str(re.search('<classification classificationScheme="company">([\s|\S]*?)</classification>', chunk))
+            curr_company_terms = re.findall('<classificationItem score="([0-9]*)">[\s|\S]*?<className>(.*?)<\/className>[\s|\S]*?<\/classificationItem>', company_chunk)
+        except:
+            curr_company_terms = []
+
+        try:
+            ticker_chunk = str(re.search('<classification classificationScheme="ticker">([\s|\S]*?)</classification>', chunk))
+            curr_ticker_terms = re.findall('<classificationItem score="([0-9]*)">[\s|\S]*?<className>(.*?)<\/className>[\s|\S]*?<\/classificationItem>', ticker_chunk)
+        except:
+            curr_ticker_terms = []
         
         headline1.append(h1)
         headline2.append(h2)
@@ -47,6 +74,10 @@ with gzip.open(file_path, 'rt') as headline_dump:
         source_filename.append(filename)
         pub_date.append(pd)
         news_wire.append(nw)
+        language.append(lang)
+        indexing_terms.append(curr_indexing_terms)
+        company_terms.append(curr_company_terms)
+        ticker_terms.append(curr_ticker_terms)
 
 print(headline1)
 print(headline2)
@@ -54,3 +85,7 @@ print(article_id)
 print(source_filename)
 print(pub_date)
 print(news_wire)
+print(language)
+print(indexing_terms)
+print(company_terms)
+print(ticker_terms)
