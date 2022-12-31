f6cefdacafb75095a61729703bdc2935f1a00fd7
Lily Bhattacharjee <lbha@ucdavis.edu>
2022-12-31 04:31:45 +0000

    Run initial tagger on CB headlines to identify POS of words other than ACQ/ACD

--- brill_tagger.py
+++ brill_tagger.py
@@ -4,6 +4,8 @@ from nltk.tag import brill, brill_trainer
 from nltk.tag import DefaultTagger, UnigramTagger, BigramTagger, TrigramTagger, BrillTaggerTrainer
 from nltk.corpus import treebank
 import pickle
+import unicodedata
+import string
 
 retrain_val = False
 
@@ -12,6 +14,25 @@ def get_initial_train_test():
     train_data = tagged_sents[:3000]
     test_data = tagged_sents[3000:]
     return train_data, test_data
+
+def clean_split_headline(headline):
+    return list(filter(None, headline.translate(str.maketrans('', '', string.punctuation)).replace('�', '').strip().lower().split(' ')))
+
+def get_crunchbase_dataset():
+    acq_cb = pd.read_csv('acq_cb.csv')
+    non_acq_cb = pd.read_csv('non_acq_cb.csv')
+
+    acq_headlines = list(acq_cb['headline'].apply(clean_split_headline))
+    acquirer = list(acq_cb['acquirer'].str.lower())
+    acquired = list(acq_cb['acquired'].str.lower())
+    non_acq_headlines = list(non_acq_cb['headline'].apply(clean_split_headline))
+    return acq_headlines, acquirer, acquired, non_acq_headlines
+
+# def add_crunchbase_acq_acd_tags(acq_headlines, acq_headlines_tagged, acquirer, acquired):
+#     for a in acq_headlines_tagged:
+
+def get_lexis_nexus_dataset():
+    pass 
 
 def backoff_tagger(train_sents, tagger_classes, backoff=None):
     for cls in tagger_classes:
@@ -64,23 +85,23 @@ def train_brill_tagger(initial_tagger, train_sents, **kwargs):
       
     return trainer.train(train_sents, **kwargs)
 
-train_data, test_data = get_initial_train_test()
+def tag_untagged_headlines(tagger, headlines):
+    tagged_headlines = []
+    for h in headlines:
+        tagged_headlines.append(tagger.tag(h))
+    return tagged_headlines
+
+# use initial tagger to identify POS of other words in crunchbase headlines
 brill_tag = get_model(retrain = retrain_val)
-print(brill_tag.accuracy(test_data))
+acq_headlines, acquirer, acquired, non_acq_headlines = get_crunchbase_dataset()
+acq_headlines_round1 = tag_untagged_headlines(brill_tag, acq_headlines)
+non_acq_headlines_round1 = tag_untagged_headlines(brill_tag, non_acq_headlines)
+print(acq_headlines_round1[:5])
+print()
+print(non_acq_headlines_round1[:5])
 
-# a = initial_tag.accuracy(test_data)
-# print ("Accuracy of Initial Tag : ", a)
-
-# use initial tagger to identify POS of other words in crunchbase headlines, while company names are fuzzy-matched as ACQ, ACD (2 new POS)
-
+# fuzzy-match company names for acq headlines as ACQ, ACD (2 custom POS)
 
 # split tagged crunchbase headlines into train + test set with POS per word
 # select a randomized large subset of LN headlines, run tagger 
 # if ACQ, AQD tags both present in tagged headline, mark as acquisition
-
-# def create_train_test():
-#     acq_cb = pd.read_csv('acq_cb.csv')
-#     non_acq_cb = pd.read_csv('non_acq_cb.csv')
-
-# train_X, train_y, test_X, test_y = create_train_test()
-# print(train_X[:5], train_y[:5])
