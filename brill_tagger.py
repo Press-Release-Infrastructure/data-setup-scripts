e8bef0b9e1775127af0cdd119ce090001c3e4677
Lily Bhattacharjee <lbha@ucdavis.edu>
2023-01-01 02:54:45 +0000

    Attempting to train/test on custom tags, but brill tagger doesn't support -- try NER models

--- brill_tagger.py
+++ brill_tagger.py
@@ -7,6 +7,12 @@ import pickle
 import unicodedata
 import string
 from fuzzywuzzy import fuzz
+import numpy as np
+import random
+import ast
+
+random.seed(0)
+np.random.seed(0)
 
 retrain_val = False
 retag = False
@@ -88,7 +94,7 @@ def backoff_tagger(train_sents, tagger_classes, backoff=None):
         backoff = cls(train_sents, backoff=backoff)
     return backoff
 
-def get_model(retrain = False):
+def get_model(filename = '', retrain = False):
     if retrain:
         train_data, test_data = get_initial_train_test()
         default_tagger = DefaultTagger('NN')
@@ -97,11 +103,11 @@ def get_model(retrain = False):
                         TrigramTagger], backoff = default_tagger)
 
         brill_tag = train_brill_tagger(initial_tag, train_data)
-        with open('sequential_backoff_tagger.pkl', 'wb') as fout:
+        with open(filename, 'wb') as fout:
             pickle.dump(brill_tag, fout)
         return brill_tag
     else:
-        f = open('sequential_backoff_tagger.pkl', 'rb')
+        f = open(filename, 'rb')
         brill_tag = pickle.load(f)
         f.close()
         return brill_tag
@@ -143,7 +149,7 @@ def tag_untagged_headlines(tagger, headlines):
 def get_acq_non_acq_cb_headlines(retag = False):
     if retag:
         # use initial tagger to identify POS of other words in crunchbase headlines
-        brill_tag = get_model(retrain = retrain_val)
+        brill_tag = get_model(filename = 'sequential_backoff_tagger.pkl', retrain = retrain_val)
 
         acq_headlines, acquirer, acquired, non_acq_headlines = get_crunchbase_dataset()
         acq_headlines_round1 = tag_untagged_headlines(brill_tag, acq_headlines)
@@ -155,13 +161,69 @@ def get_acq_non_acq_cb_headlines(retag = False):
 
         # fuzzy-match company names for acq headlines as ACQ, ACD (2 custom POS)
         add_crunchbase_acq_acd_tags(acq_headlines, acq_headlines_round1, acquirer, acquired, 'acq_headlines_tagged.csv')
-    acq_headlines_tagged = list(pd.read_csv('acq_headlines_tagged.csv')['tagged'])
-    non_acq_headlines_tagged = list(pd.read_csv('non_acq_headlines_tagged.csv')['tagged'])
+    acq_headlines_tagged = list(pd.read_csv('acq_headlines_tagged.csv')['tagged'].apply(ast.literal_eval))
+    non_acq_headlines_tagged = list(pd.read_csv('non_acq_headlines_tagged.csv')['tagged'].apply(ast.literal_eval))
     return acq_headlines_tagged, non_acq_headlines_tagged
+
+def split_train_test_cb(acq_headlines_tagged, non_acq_headlines_tagged):
+    # select randomized 80% of each (acq, non acq) for train, rest for test
+    acq_headlines_tagged, non_acq_headlines_tagged = np.array(acq_headlines_tagged, dtype = object), np.array(non_acq_headlines_tagged, dtype = object)
+    m, n = len(acq_headlines_tagged), len(non_acq_headlines_tagged)
+    
+    num_acq_train = int(0.80 * m)
+    num_non_acq_train = int(0.80 * n)
+    num_acq_test = m - num_acq_train
+    num_non_acq_test = n - num_non_acq_train
+    
+    acq_train_idxes = np.random.choice(np.arange(m), num_acq_train, replace = False)
+    acq_test_idxes = np.array(list(set(np.arange(m)) - set(acq_train_idxes)))
+    
+    non_acq_train_idxes = np.random.choice(np.arange(n), num_non_acq_train, replace = False)
+    non_acq_test_idxes = np.array(list(set(np.arange(n)) - set(non_acq_train_idxes)))
+    
+    acq_train = acq_headlines_tagged[acq_train_idxes]
+    acq_test = acq_headlines_tagged[acq_test_idxes]
+
+    non_acq_train = non_acq_headlines_tagged[non_acq_train_idxes]
+    non_acq_test = non_acq_headlines_tagged[non_acq_test_idxes]
+
+    return acq_train, acq_test, non_acq_train, non_acq_test
+
+def shuffle_data(acq_train, acq_test, non_acq_train, non_acq_test):
+    # train_data = np.array(list(acq_train) + list(non_acq_train), dtype = object)
+    # test_data = np.array(list(acq_test) + list(non_acq_test), dtype = object)
+    train_data = np.array(acq_train)
+    test_data = np.array(acq_test)
+    np.random.shuffle(train_data)
+    np.random.shuffle(test_data)
+    return list(train_data), list(test_data)
 
 acq_headlines_tagged, non_acq_headlines_tagged = get_acq_non_acq_cb_headlines(retag = False)
 
 # split tagged crunchbase headlines into train + test set with POS per word
+acq_train, acq_test, non_acq_train, non_acq_test = split_train_test_cb(acq_headlines_tagged, non_acq_headlines_tagged)
+train_data, test_data = shuffle_data(acq_train, acq_test, non_acq_train, non_acq_test)
+retagged_train_data = pd.DataFrame({'tagged': train_data})
+retagged_train_data.to_csv('retagged_train_data.csv')
+print(train_data[0])
+print(test_data[0])
+
+# train a new brill tagger model on the retagged data
+retagged_model = get_model(filename = 'retagged_backoff_tagger.pkl', retrain = False)
+
+# what is the accuracy on test data?
+print(retagged_model.accuracy(test_data))
+
+# to sanity-check, tag test data, save to csv
+untagged_test_data = []
+for h in test_data:
+    untagged_test_data.append(nltk.tag.untag(h))
+tagged_test = tag_untagged_headlines(retagged_model, untagged_test_data)
+retagged_test_data = pd.DataFrame({'tagged': tagged_test})
+retagged_test_data.to_csv('retagged_test_data.csv')
+print(tagged_test[:5])
+print()
+print(retagged_model.rules())
 
 # select a randomized large subset of LN headlines, run tagger 
 # if ACQ, AQD tags both present in tagged headline, mark as acquisition
